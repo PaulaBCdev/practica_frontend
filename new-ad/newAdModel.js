ba182dc06f6d2320be53eedc4c0b41649cb0583a
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-04-15 13:02:16 +0200

    create new ad with photo

--- new-ad/newAdModel.js
+++ new-ad/newAdModel.js
@@ -2,26 +2,43 @@
 export async function newAd(productName, productDescription, productPrice, adType, productImage) {
     const token = localStorage.getItem("token")
 
-    try {
-        const response = await fetch("http://localhost:8000/api/products", {
+    let image = null
+
+    if (productImage) {
+        const formData = new FormData()
+        formData.append('file', productImage)
+
+        const response = await fetch("http://localhost:8000/upload", {
             method: "POST",
-            body: JSON.stringify({
-                name: productName,
-                description: productDescription,
-                price: productPrice,
-                type: adType
-            }),
+            body: formData,
             headers: {
-                "Content-type": "application/json",
                 "Authorization": `Bearer ${token}`
             }
         })
 
-        if (!response.ok) {
-            throw new Error("New ad creation failed. Try again later.")
+        const imageJson = await response.json()
+        image = imageJson.path
+    }
+
+    console.log(image)
+
+    const response = await fetch("http://localhost:8000/api/products", {
+        method: "POST",
+        body: JSON.stringify({
+            name: productName,
+            description: productDescription,
+            price: productPrice,
+            type: adType,
+            photo: image
+        }),
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${token}`
         }
-    } catch (e) {
-        console.log(e)
+    })
+
+    if (!response.ok) {
+        throw new Error("New ad creation failed. Try again later.")
     }
 
 }
