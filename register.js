cb588618d029b1ea3b15e5274283bf2ff5657f45
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-04-23 18:56:04 +0200

    register page design with responsive

--- register.js
+++ register.js
@@ -1,11 +1,13 @@
 import { loaderController } from './loader/loaderController.js'
 import { registerController } from "./register/registerController.js";
 import { notificationsController } from './notifications/notificationsController.js'
+import { sessionController } from './session/sessionController.js'
 
 document.addEventListener("DOMContentLoaded", () => {
     const registerForm = document.querySelector('form')
     const loader = document.querySelector('.loader')
     const notifications = document.querySelector('.notifications')
+    const session = document.querySelector(".session")
     const { show, hide } = loaderController(loader)
     const { showNotification } = notificationsController(notifications)
 
@@ -29,4 +31,5 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     registerController(registerForm)
+    sessionController(session)
 })
