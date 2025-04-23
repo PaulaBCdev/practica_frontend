66e6254f8557fb921c965a74d2f6dcb96e1697c0
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-04-23 19:11:14 +0200

    login page design with responsive and register update

--- login.js
+++ login.js
@@ -1,11 +1,13 @@
 import { loaderController } from './loader/loaderController.js'
 import { loginController } from "./login/loginController.js";
 import { notificationsController } from "./notifications/notificationsController.js";
+import { sessionController } from './session/sessionController.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('form')
     const loader = document.querySelector('.loader')
     const notifications = document.querySelector('.notifications')
+    const session = document.querySelector(".session")
     const { show, hide } = loaderController(loader)
     const { showNotification } = notificationsController(notifications)
 
@@ -23,4 +25,5 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     loginController(loginForm)
+    sessionController(session)
 })
